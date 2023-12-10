230470d6e7fcc90e00d7a63aac829a56b88d886a
naumchuk0 <mn645680@gmail.com>
2023-12-10 15:52:30 +0000

    create php-site

---
+++ config/connection_database.php
@@ -0,0 +1,20 @@
+<?php
+
+try {
+    $host = 'local.vpd111.com';
+    $database = 'local.vpd111.com';
+    $username = 'root';
+    $password = '';
+
+    $dsn = "mysql:host=$host;dbname=$database;charset=utf8";
+
+    $options = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ];
+    $pdo = new PDO($dsn, $username, $password, $options);
+    //echo "<h3>Connection Success</h3>";
+
+} catch (PDOException $e) {
+    die('Connection failed: ' . $e->getMessage());
+}
